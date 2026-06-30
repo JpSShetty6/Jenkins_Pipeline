5ed430255b2f63aa6125df2502efe38f3ed5d5ac
JAYAPRAKASH <jaiprakashsshetty@gmail.com>
2026-06-30 06:26:58 +0000

    updated maven app files

---
+++ Java_Maven/src/main/java/com/example/App.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class App {
+
+    public static String getMessage() {
+        return "Hello, Jenkins Maven Pipeline!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getMessage());
+    }
+}
--- Java_Maven/src/test/java/com/example/AppTest.java
+++ Java_Maven/src/test/java/com/example/AppTest.java
@@ -1,16 +1,12 @@
 package com.example;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
 
     @Test
-    public void testAdd() {
-
-        assertEquals(30, App.add(10,20));
-
+    public void testMessage() {
+        assertEquals("Hello, Jenkins Maven Pipeline!", App.getMessage());
     }
-
 }
